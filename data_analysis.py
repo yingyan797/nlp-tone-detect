3756ada5d6be6b0822c9c653c002d9218499cf78
Xiwei Gao <xg1020@ic.ac.uk>
2025-02-12 16:49:04 +0000

    Some ideas of graphs in comments

--- data_analysis.py
+++ data_analysis.py
@@ -7,3 +7,16 @@ import config
 with open(config.train_data_path, 'r', newline='', encoding='utf-8') as train_file:
     train_set_reader = csv.reader(train_file, delimiter='\t')
 
+# Bar graph of label distribution
+
+# Bar graph of text length distribution
+
+# Bar graph of contry code distribution
+
+# Bar graph of key word distribution
+
+# Grouped bar graph of label distribution by text length
+
+# Grouped bar graph of label distribution by country code
+
+# Grouped bar graph of label distribution by key word
