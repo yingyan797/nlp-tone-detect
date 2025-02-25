b432803838d3fe5d08e3218e8e412a2cebd44d75
Xiwei Gao <xg1020@ic.ac.uk>
2025-02-25 20:58:33 +0000

    Updates run script

--- train_script.sh
+++ train_script.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-
-/vol/bitbucket/xg1020/nlp-tone-detect/.venv/bin/pip install -r /vol/bitbucket/xg1020/nlp-tone-detect/requirements.txt
-
-. /vol/cuda/12.0.0/setup.sh
-/vol/bitbucket/xg1020/nlp-tone-detect/.venv/bin/python /vol/bitbucket/xg1020/nlp-tone-detect/pipeline.py
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sk2324
+#SBATCH --mail-user=ac2921
+#SBATCH --mail-user=xg1020
+source .venv/bin/activate
+source /vol/cuda/12.4.0/setup.csh
+nvidia-smi
+nvcc --version
+pip install -r /vol/bitbucket/xg1020/nlp-tone-detect/requirements.txt
+python /vol/bitbucket/xg1020/nlp-tone-detect/pipeline.py
