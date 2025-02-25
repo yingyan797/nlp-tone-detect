1da4fea140ddc51802436c12cc0b0a06cd793f61
Xiwei Gao <xg1020@ic.ac.uk>
2025-02-25 18:49:05 +0000

    Improved training script

--- train_script.sh
+++ train_script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+/vol/bitbucket/xg1020/nlp-tone-detect/.venv/bin/pip install -r /vol/bitbucket/xg1020/nlp-tone-detect/requirements.txt
+
 . /vol/cuda/12.0.0/setup.sh
 /vol/bitbucket/xg1020/nlp-tone-detect/.venv/bin/python /vol/bitbucket/xg1020/nlp-tone-detect/pipeline.py
